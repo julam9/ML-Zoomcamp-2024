613e02f4a594bbb5c826abf0a8a5cbc7124f4f80
Julio Antonio <106480599+julam9@users.noreply.github.com>
2024-10-29 21:32:47 +0700

    Delete Week-5/HW-5/download_model.py

--- Week-5/HW-5/download_model.py
+++
@@ -1,5 +0,0 @@
-import urllib.request
-
-PREFIX = "https://raw.githubusercontent.com/DataTalksClub/machine-learning-zoomcamp/master/cohorts/2024/05-deployment/homework/"
-urllib.request.urlretrieve(PREFIX + "model1.bin", "model1.bin")
-urllib.request.urlretrieve(PREFIX + "dv.bin", "dv.bin")
